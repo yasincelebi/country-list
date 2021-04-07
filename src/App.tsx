3e11a63b94ed05411be72c0655e5046d7e755d28
yasincelebi <yasincelebix@gmail.com>
2021-04-07 04:20:19 +0300

    Add components and classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CountryList from "./components/CountryList";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-dark pt-3">
+      <div className="container font-weight-bold">
+        <Header />
+        <CountryList />
+      </div>
     </div>
   );
 }
